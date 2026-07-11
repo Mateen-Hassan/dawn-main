00e5d3fa347b6e5c1ef7e7ae7d5515abb0b19fd7
Mateen-Hassan <mmehdi12141214@gmail.com>
2026-07-11 12:18:25 +0500

    asdasd

--- assets/variant-picker.js
+++ assets/variant-picker.js
@@ -1,67 +1,59 @@
-class variantPicker extends HTMLElement {
-    constructor() {
-        super();
-        this.isLoading = false;
+class variantPicker extends HTMLElement{
+    constructor(){
+       super();
+    }   
+   
+    get sectionId(){
+       return this.dataset.sectionId;
     }
+   
+    connectedCallback(){
+       this.variantPicker=this.querySelectorAll('input[type="radio"]')
+   
+       this.handleChange=this.handleChange.bind(this);
+       this.variantPicker.forEach((picker)=>{
+        picker.addEventListener('change', this.handleChange);
+       })
+   }
+   disconnectedCallback(){
+    this.variantPicker.forEach((picker)=>{
+        picker.removeEventListener('change', this.handleChange);
+       })
+   }
+   handleChange(){
+       const optionValueIds = Array.from(
+         this.querySelectorAll('fieldset input[type="radio"]:checked')
+       )
+         .map((input) => input.dataset.optionValueId)
+         .filter(Boolean);
 
-    get sectionId() {
-        return this.dataset.sectionId;
-    }
+       const params = new URLSearchParams();
+       if (optionValueIds.length) {
+         params.set('option_values', optionValueIds.join(','));
+       }
+       params.set('section_id', this.sectionId);
 
-    connectedCallback() {
-        this.variantPicker = this.querySelector('select[name="id"]');
-        
-        if (!this.variantPicker) {
-            console.warn('No select element with name="id" found');
-            return;
-        }
+       const url = `${window.location.pathname}?${params.toString()}`;
 
-        this.variantPicker.addEventListener('change', this.handleChange.bind(this));
-    }
+       fetch(url)
+       .then((response) => response.text())
+       .then((html) => {
+           const tempDiv = document.createElement('div');
+           tempDiv.innerHTML = html;
 
-    handleChange(event) {
-        const select = event.currentTarget;
-        
-        // Prevent duplicate requests
-        if (this.isLoading) return;
-        this.isLoading = true;
-        select.disabled = true;
+           document.querySelector('.container').innerHTML =
+             tempDiv.querySelector('.container').innerHTML;
 
-        const url = `${window.location.pathname}?variant=${select.value}&section_id=${this.sectionId}`;
+           const variantId = tempDiv.querySelector('input[name="id"]')?.value;
+           const newUrl = new URL(window.location.pathname, window.location.origin);
+           if (variantId) {
+             newUrl.searchParams.set('variant', variantId);
+           }
+           window.history.pushState({}, '', newUrl.toString());
+       });
+   }
+   }
+   
+   customElements.define("variant-picker", variantPicker)
 
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.text();
-            })
-            .then(html => {
-                const tempDiv = document.createElement('div');
-                tempDiv.innerHTML = html;
-
-                const newContainer = tempDiv.querySelector('.container');
-                const currentContainer = document.querySelector('.container');
-                
-                if (newContainer && currentContainer) {
-                    // More efficient: replace children only
-                    currentContainer.innerHTML = newContainer.innerHTML;
-                } else {
-                    console.warn('Container not found in response');
-                }
-                const newUrl= new URL(url,window.location.origin)
-                newUrl.searchParams.delete('section_id')
-                window.history.pushState({},"",newUrl.toString())
-            })
-            .catch(error => {
-                console.error('Error fetching variant:', error);
-                // Optionally show user feedback
-            })
-            .finally(() => {
-                this.isLoading = false;
-                select.disabled = false;
-            });
-    }
-}
-
-customElements.define('variant-picker', variantPicker);
+  
